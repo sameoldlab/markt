c5fe4c4f345da1b61494e6213b6e653c6dd358e1
ibro <sameoldlab@proton.me>
2025-01-05 23:19:17 +0100

    signin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { client } from '$lib/initializeClient';
+	import { agent } from '$lib/agent.svelte';
+
+	let username = $state('');
+	const signin = async (e: SubmitEvent) => {
+		e.preventDefault();
+		await client.signIn(username);
+	};
+</script>
+
+{#if agent.value === null}
+	<form onsubmit={signin}>
+		<label>
+			username
+			<input type="text" required name="handle" bind:value={username} />
+		</label>
+		<button type="submit">Log In</button>
+	</form>
+{/if}
